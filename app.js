aeb06591dc79d8108f6c8492e0a3a2a914cad8cc
Sarah-Opalenik <sarahmo93@gmail.com>
2019-06-13 16:55:55 -0600

    made pretty

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let money = 0;
+let foodBank = 10;
+
 let store = [
   {
     name: "House",
@@ -9,7 +11,8 @@ let store = [
     cost: 1000
   },
   {
-
+    name: "Animal Feed",
+    cost: 10
   }
 ]
 let animals = [
@@ -18,45 +21,46 @@ let animals = [
     feed: 0,
     food: "seeds",
     value: 10,
-    // image: "https://m.worldanimalfoundation.net/i/chicken3.jpg"
+    image: "/images/chicken.jpg"
   },
   {
     name: "Cow",
     feed: 0,
     food: "grass",
     value: 20,
-    // image: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzAwNy80MjEvb3JpZ2luYWwvMDUwNzExX2dlbmVyaWNfY293XzAyLmpwZw=="
+    image: "/images/cow2.jpg"
   },
   {
     name: "Goat",
     feed: 0,
     food: "grass",
     value: 30,
-    // image: "https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg"
+    image: "/images/goat.jpg"
   },
   {
     name: "Pig",
     feed: 0,
     food: "leftovers",
     value: 40,
-    // image: "https://cdni.rt.com/files/2018.04/article/5ae1a7b6fc7e93bb6e8b457f.jpg"
+    image: "/images/pig3.jpg"
   }
 ]
 
 function drawFarm() {
   let animalsRow = document.querySelector("#animal-control")
-
+  let feedGet = document.querySelector("#feed-count")
   let moneyGet = document.querySelector("#money-count")
   let farmTemplate = "";
   for (let index = 0; index < animals.length; index++) {
     let animal = animals[index];
     farmTemplate += `
-    <div class="col">
+    <div class="col pt-5">
       <h2>${animal.name}</h2>
+      <img src="${animal.image}" alt="some text" class="rounded">
       <p>Feed: ${animal.feed} ${animal.food}</p>
       <p>Harvest: $ ${animal.value} </p>
-      <button class="btn btn-warning" onclick="feed(${index})">Feed</button>
-      <button class="btn btn-success" onclick="harvest(${index})">Harvest</button>
+      <button type = "button" class="btn btn-warning" onclick="feed(${index})">Feed</button>
+      <button type = "button" class="btn btn-success" onclick="harvest(${index})">Harvest</button>
     </div>
     `
   }
@@ -66,19 +70,21 @@ function drawFarm() {
     let storeItem = store[stIndex];
     storeTemplate += `
       <div class="col">
-        <button class="btn btn-primary" onclick="purchase(${stIndex})">Purchase ${storeItem.name}</button>
+        <button type= "button" class="btn btn-primary" onclick="purchase(${stIndex})">Purchase ${storeItem.name}</button>
       </div>
     `
 
   }
   storeRow.innerHTML = storeTemplate;
   animalsRow.innerHTML = farmTemplate;
-  moneyGet.textContent = money.toString()
+  moneyGet.textContent = money.toString();
+  feedGet.textContent = foodBank.toString();
 }
 
 function feed(i) {
   let animal = animals[i];
   animal.feed++;
+  foodBank--;
   drawFarm();
 }
 
@@ -86,14 +92,16 @@ function harvest(i) {
   let animal = animals[i];
   money += animal.value * animal.feed
   animal.feed = 0;
-  // animal.value += animal.value * animal.feed
   drawFarm()
 
 }
 
 function purchase(s) {
   let storeItem = store[s];
-  money -= storeItem.cost
+  money -= storeItem.cost;
+  if (storeItem = store[2]) {
+    foodBank += 5;
+  }
   drawFarm()
 }
 
